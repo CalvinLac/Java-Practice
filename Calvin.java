015a20706d0f064dbb49f95f1b08be18066cf0d2
Calvin <Calvin.lacc@gmail.com>
2016-09-17 15:36:52 -0600

    Working with simple operators in Java

--- Calvin.java
+++ Calvin.java
@@ -1,15 +1,25 @@
+import java.util.Scanner;
+
 public class Calvin
 {
   public static void main(String[] args)
   {
     int numberofeggs, numberofbaskets;
-    numberofeggs=6; 
-    numberofbaskets = 10;
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("Enter the amount of eggs in each basket, bud");
+    numberofeggs = keyboard.nextInt();
+    System.out.println("Enter the amount of baskets, bud");
+    numberofbaskets = keyboard.nextInt();
     System.out.println("If you have");
     System.out.println(numberofeggs + "eggs per basket");
     System.out.println(numberofbaskets + "baskets, then");
     System.out.println("the total number of eggs is" + 
       (numberofeggs*numberofbaskets));
+
+    System.out.println("When you take 2 eggs away from each basket");
+    numberofeggs = numberofeggs - 2;
+    System.out.println("There are" + numberofeggs + "per basket");
+    System.out.println("And there are" + (numberofeggs * numberofbaskets) + "eggs in total");
 
   }
 }
