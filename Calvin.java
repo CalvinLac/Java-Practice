82b07c59bc98c17a7e68603f0a73fdc4929c10af
Calvin <Calvin.lacc@gmail.com>
2016-09-17 15:19:07 -0600

    Learning simple snytax in java

---
+++ Calvin.java
@@ -0,0 +1,15 @@
+public class Calvin
+{
+  public static void main(String[] args)
+  {
+    int numberofeggs, numberofbaskets;
+    numberofeggs=6; 
+    numberofbaskets = 10;
+    System.out.println("If you have");
+    System.out.println(numberofeggs + "eggs per basket");
+    System.out.println(numberofbaskets + "baskets, then");
+    System.out.println("the total number of eggs is" + 
+      (numberofeggs*numberofbaskets));
+
+  }
+}
