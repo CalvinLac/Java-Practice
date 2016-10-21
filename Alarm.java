a775d205ad17758f89469c9de376ffbcbef8d06a
Calvin <Calvin.lacc@gmail.com>
2016-10-20 22:12:27 -0600

    Working on an alarm in java

---
+++ Alarm.java
@@ -0,0 +1,19 @@
+public class Alarm {
+
+  public int second;
+  public int minute;
+  public int hour;
+
+  Alarm () {
+    second = 0;
+    minute = 0; 
+    hour = 0;
+  }
+
+  Alarm (int sec, int min, int hr) {
+    second = sec;
+    minute = min;
+    hour = hr;
+  }
+
+}
---
+++ AlarmTest.java
@@ -0,0 +1,6 @@
+public class AlarmTest {
+  public static void main(String args[]) {
+    Alarm hi = new Alarm();
+    System.out.println(hi.second);
+  }
+}
